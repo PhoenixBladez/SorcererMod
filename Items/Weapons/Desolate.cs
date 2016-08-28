d8935cae73617624d21ef4f62330117149624c06
VoxelMC <voxelmc@hotmail.com>
2016-08-28 08:19:10 -0700

    Added Desolation and Sandstorm projectile

---
+++ Items/Weapons/Desolate.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using System;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SorcererMod.Items.Weapons
+{
+    public class Desolate : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Desolation"; //slightly changed the name :P
+            item.damage = 17;
+            item.magic = true;
+            item.mana = 9;
+            item.width = 46;
+            item.height = 46;
+            item.useTime = 7;
+            item.useAnimation = 27;
+            item.useStyle = 5;
+            Item.staff[item.type] = true;
+            item.noMelee = true;
+            item.knockBack = 0;
+            item.value = 20000;
+            item.rare = 2;
+            item.useSound = 34;
+            item.autoReuse = false;
+            item.shoot = mod.ProjectileType("Sandstorm");
+            item.shootSpeed = 20f;
+            item.toolTip = "Fires a sandstorm at your enemies";
+            item.autoReuse = true;
+        }
+        
+
+        public override void AddRecipes()
+        {
+            //UUUHHHHH
+        }
+    }
+}
