72d48dc4922dbe02ccd1026092caa9aa03ce5705
PhoenixBladez <aryanandabhay@gmail.com>
2016-08-28 10:50:41 -0700

    Update IceStaff.cs

--- Items/Weapons/IceStaff.cs
+++ Items/Weapons/IceStaff.cs
@@ -13,7 +13,7 @@ namespace SorcererMod.Items.Weapons
             item.name = "Ice Staff";
             item.damage = 18;
             item.magic = true;
-            item.mana = 8;
+            item.mana = 15;
             item.width = 46;
             item.height = 46;
             item.useTime = 28;
