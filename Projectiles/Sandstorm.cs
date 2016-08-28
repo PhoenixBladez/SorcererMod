52300ab83621685d2c5b5304f82a7accff3aa7cf
PhoenixBladez <aryanandabhay@gmail.com>
2016-08-28 10:46:50 -0700

    Update Sandstorm.cs

--- Projectiles/Sandstorm.cs
+++ Projectiles/Sandstorm.cs
@@ -15,7 +15,7 @@ namespace SorcererMod.Projectiles
             projectile.friendly = true;
             projectile.magic = true;
             projectile.width = 10; projectile.height = 10;
-            projectile.penetrate = 0;
+            projectile.penetrate = 1;
             projectile.alpha = 255;
             projectile.timeLeft = 30;
         }
