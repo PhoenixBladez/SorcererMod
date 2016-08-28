67ff00224d89764164b39766935c2492f4e90313
VoxelMC <voxelmc@hotmail.com>
2016-08-28 16:58:41 -0700

    Added a few thingos

---
+++ Items/Materials/EssenceOfTheWorld.cs
@@ -0,0 +1,22 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SorcererMod.Items.Materials
+{
+    public class EssenceOfTheWorld : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Essence of the World";
+            item.width = 30;
+            item.height = 30;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.value = 50;
+            item.scale = 0.6f;
+        }
+    }
+}
