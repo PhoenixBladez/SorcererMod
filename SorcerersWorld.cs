d5853e3d64eaa126378246ab6d45c3e1b5ab8e1e
GraydeeSi <wiseguy0002@gmail.com>
2016-09-04 13:14:54 -0400

    Now does postgen

--- SorcerersWorld.cs
+++ SorcerersWorld.cs
@@ -12,23 +12,29 @@ namespace SorcererMod
 {
     public class SorcerersWorld : ModWorld
     {
-
-        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+		public static bool Magicite = false;
+ public override void Initialize()
         {
-			int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
-			if (ShiniesIndex != -1)
+            if (NPC.downedBoss1 == true)
+            {
+                Magicite = true;
+            }
+            else
+            {
+                Magicite = false;
+            }
+		}
+        public override void PostUpdate() 
+	{
+            if (NPC.downedBoss1 == true)
+            {
+			if (Magicite == false)
 			{
-				tasks.Insert(ShiniesIndex + 1, new PassLegacy("Example Mod Ores", delegate (GenerationProgress progress)
-				{
-					progress.Message = "Adding magicite";
-
-					for (int k = 0; k < (int)((double)(Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)
+			Magicite = true;
+			Main.NewText("Magicite has graced your land", Color.Orange.R, Color.Orange.G, Color.Orange.B);
+			for (int k = 0; k < (int)((double)(Main.maxTilesX * Main.maxTilesY) * (6E-05) / 2); k++)
 					{
 						WorldGen.TileRunner(WorldGen.genRand.Next(0, Main.maxTilesX), WorldGen.genRand.Next((int)WorldGen.rockLayer, Main.maxTilesY), (double)WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 6), mod.TileType("MagiciteOre"), false, 0f, 0f, false, true);
 					}
-				}));
-			}
-
-        } 
     }  
 }  
